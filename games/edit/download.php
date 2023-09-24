665c50b8b9eeab00a22fff1b79518554b4adb314
LukeDevsE <108841495+LukeDevsE@users.noreply.github.com>
2023-09-24 09:48:46 -0600

    Update download.php

--- games/edit/download.php
+++ games/edit/download.php
@@ -1,6 +1,6 @@
 <?php
 $userid = $_GET['id'];
-$link = mysqli_connect("s1.ct8.pl","m29812_config_gr","W5U.+j-9eo-T_J2Vfd/arDy1u9vEN0","m29812_gr");
+$link = mysqli_connect("s1.ct8.pl","USER","PASS","SERVER");
 if (!isset($userid)) {
     die("An Error Ocurred");  
 }
